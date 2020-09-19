1ebd452e669dfc0821034e2fdf5899f3f63b90fa
Satoshi Kobayashi <pikanchisato@yahoo.co.jp>
2020-09-19 14:48:42 +0900

    Initial Commit

---
+++ TamagotchiPet/TamagotchiPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiPetApp.swift
+//  TamagotchiPet
+//
+//  Created by MacBook Pro on 2020/09/19.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
